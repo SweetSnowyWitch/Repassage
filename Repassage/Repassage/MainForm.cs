20a701a37d3de3ee454b34cca8b1acff3dc00ae9
SweetSnowyWitch <snowysatan@yandex.ru>
2021-04-14 03:51:41 +0500

    Test Changes

---
+++ Repassage/Repassage/MainForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Repassage
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            Size = new Size(1920, 1080);
+            MaximizeBox = false;
+            WindowState = FormWindowState.Maximized;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
